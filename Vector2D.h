4a93bbd397aee004c1f119a5034592f6415906f1
Ferhat Tumer <ferhat.tumer@takosan_domain>
2020-04-16 09:28:59 +0300

    Proje dosyası ekle.

---
+++ Vector2D.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace math
+{
+
+	class Vector2D
+	{
+	public:
+		float myfirst;
+		float mysecond;
+
+		Vector2D(float first = 0.0f, float second = 0.0f) : myfirst(first), mysecond(second) {}
+	};
+
+	Vector2D operator+(const Vector2D& left, const Vector2D& right)
+	{
+		return Vector2D(left.myfirst + right.myfirst, left.mysecond + right.mysecond);
+	}
+
+	Vector2D operator*(float scalar, const Vector2D& vector)
+	{
+		return Vector2D(vector.myfirst * scalar, vector.mysecond * scalar);
+	}
+
+	Vector2D operator*(const Vector2D& vector, float scalar )
+	{
+		return Vector2D(vector.myfirst * scalar, vector.mysecond * scalar);
+	}
+
+}
